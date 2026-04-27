7c2a3b9005ed9c6e9e215cefdc9820b6aadbc5e3
khongkoemtong <kkoemtong@gmail.com>
2026-04-27 11:40:42 +0700

    first day

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-
-@app.route('/service')
-def good():
-    return render_template('service.html')
 
 if __name__=="__main__":
     app.run(debug=True)
-
-# 6 => home,about ,service ,contact ,login ,register
