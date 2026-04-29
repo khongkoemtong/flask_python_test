3fda97ae78819809d56bce4356907dc6506ea37d
khongkoemtong <kkoemtong@gmail.com>
2026-04-29 11:27:25 +0700

    pymysql

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask 
+from flask import Flask ,render_template
+import pymysql
 
 app = Flask(__name__)
 
 
+def connect_db ():
+    connection  = pymysql.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        db="reaksa_piseth_db"
+
+    )
+
+    if not connection:
+        print("can not connect to database ! 🥺🥺")
+    else:
+        print("database connected ! ⭐⭐🥳")    
+
+
+connect_db()
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__=="__main__":
     app.run(debug=True)
