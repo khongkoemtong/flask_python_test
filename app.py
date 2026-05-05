50917f948b143084aab760cedcfa3563999d2cfd
khongkoemtong <kkoemtong@gmail.com>
2026-05-05 12:04:48 +0700

    delete update

--- app.py
+++ app.py
@@ -31,16 +31,49 @@ def insert ():
     conection = connect_db()
     cursor = conection.cursor()
 
-
     name = request.form['name']
     age = request.form['age']
     gender = request.form['gender']
     grade = request.form['grade']
 
-
     sql = "INSERT INTO students (name,age,gender,grade) values (%s,%s,%s,%s)"
 
     cursor.execute(sql,(name,age,gender,grade))
+    conection.commit()
+    
+    return redirect(url_for('hello'))
+
+
+@app.route('/update',methods=['POST'])
+def update ():
+    conection = connect_db()
+    cursor = conection.cursor()
+
+    id = request.form['update_id']
+    name = request.form['update_name']
+    age = request.form['update_age']
+    gender = request.form['update_gender']
+    grade = request.form['update_grade']
+
+    sql = "UPDATE students SET name = %s , age = %s ,gender = %s ,grade =%s WHERE id= %s"
+
+    cursor.execute(sql,(name,age,gender,grade,id))
+
+    conection.commit()
+
+    return redirect(url_for("hello"))
+
+
+@app.route('/delete' ,methods=['POST'])
+def delete ():
+    conection = connect_db()
+    cursor = conection.cursor()
+
+    id = request.form['delete_id']
+
+    sql = "DELETE FROM students WHERE id = %s"
+
+    cursor.execute(sql,(id))
 
     conection.commit()
 
